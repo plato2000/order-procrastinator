8ef3adda2399e8203bedd86d5d3ae606db4bf4a5
Parth Oza <plusparth@gmail.com>
2019-09-27 11:09:44 -0400

    Minor change: use variable instead of modifying array

--- assets/js/read-carts.js
+++ assets/js/read-carts.js
@@ -15,18 +15,19 @@ $(function() {
     if (window.location.href.indexOf("vexrobotics.com/checkout/cart") >= 0) {
         var items = [];
         $("#shopping-cart-table tbody tr").each(function() {
-            items.push(new Product());
+            var p = new Product();
             $.each(this.cells, function() {
                 if ($(this).hasClass("product-cart-info")) {
                     // console.log("in correct td");
-                    items[items.length - 1].name = $.trim($(this).children("h2.product-name").text());
-                    items[items.length - 1].number = $.trim($(this).children("div.product-cart-sku").text()).substring(5);
+                    p.name = $.trim($(this).children("h2.product-name").text());
+                    p.number = $.trim($(this).children("div.product-cart-sku").text()).substring(5);
                 } else if ($(this).hasClass("product-cart-price")) {
-                    items[items.length - 1].price = $.trim($(this).children("span.cart-price").text());
+                    p.price = $.trim($(this).children("span.cart-price").text());
                 } else if ($(this).hasClass("product-cart-actions")) {
-                    items[items.length - 1].quantity = $(this).children("input").val();
+                    p.quantity = $(this).children("input").val();
                 }
             });
+            items.push(p);
         });
         console.log(items);
     }
